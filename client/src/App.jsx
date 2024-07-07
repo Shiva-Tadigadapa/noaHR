4d8e151ef6b564d56e3f852c18cd001f7e6998f8
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-07-08 04:02:06 +0530

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard/DashBoard';
 import NewJobPost from './pages/NewJob/NewJobPost';
 import { Toaster } from 'sonner';
 import Sidebar from './components/dashBoard/SideBar';
@@ -12,7 +11,6 @@ const App = () => {
       <Sidebar />
         {/* <h1 className='text-4xl'>App</h1> */}
         <Routes>
-          <Route path="/" element={<Dashboard />} />
           <Route path="/newjob" element={<NewJobPost />} />
         </Routes>
         <Toaster position='top-right' richColors/>
