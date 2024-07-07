3c8c6cb084bd95db24f5abfcb3f45fd93dc44553
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-07-08 04:03:40 +0530

    resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard/DashBoard'
 import NewJobPost from './pages/NewJob/NewJobPost';
 import { Toaster } from 'sonner';
 import Sidebar from './components/dashBoard/SideBar';
@@ -11,6 +12,7 @@ const App = () => {
       <Sidebar />
         {/* <h1 className='text-4xl'>App</h1> */}
         <Routes>
+          <Route path="/" element={<Dashboard />} />
           <Route path="/newjob" element={<NewJobPost />} />
         </Routes>
         <Toaster position='top-right' richColors/>
---
+++ client/src/pages/Dashboard/DashBoard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Sidebar from "../../components/dashBoard/SideBar";
+import SearchHOC from "../../components/dashBoard/SearchHoc";
+import Avatar from "boring-avatars";
+import JobCard from "../../components/dashBoard/JobCard";
+import Header from "../../components/dashBoard/Header";
+const Dashboard = () => {
+  return (
+    <>
+      <div className=" w-[100%] gap-5 px-4 py-4 flex justify-center    items-center">
+        <div className=" px-4 py-5  rounded-3xl bg-gray-100 border w-full ">
+          <SearchHOC />
+
+          <div>
+            <Header />
+          </div>
+
+          <div className=" flex flex-col gap-4">
+            <JobCard />
+            <JobCard />
+            <JobCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
