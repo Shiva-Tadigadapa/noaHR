b69e0a5860457667ee007da7af6debdba11f189e
p-1000 <patchikarla.12108354@lpu.in>
2024-07-08 19:12:14 +0530

    docs: add careers page
    Adds a Careers page to the application.
    
    The Careers page includes a banner image, a description of the company, a list of open job positions, and a form for applicants to submit their information.

---
+++ client/src/pages/Careers/Career.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Index from "../../components/Careers/Index";
+import Footer from "../../components/Careers/Footer";
+
+const Career = () => {
+  return (
+    <>
+    <div className="flex flex-col">
+    <Index />
+    <Footer />
+    </div>
+    </>
+  );
+};
+
+export default Career;
