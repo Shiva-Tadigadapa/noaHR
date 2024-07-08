1bda9df9c002013faab28e2da96d94793bc45963
shiva <shivatadigadapa@gmail.com>
2024-07-08 16:35:28 +0530

    done-3

--- client/src/pages/dashboard/dashBoard.jsx
+++ client/src/pages/dashboard/dashBoard.jsx
@@ -1,32 +1,13 @@
-import React from "react";
-import Sidebar from "../../components/dashBoard/SideBar";
-import SearchHOC from "../../components/dashBoard/SearchHoc";
-import Avatar from "boring-avatars";
-import JobCard from "../../components/dashBoard/JobCard";
-import Header from "../../components/dashBoard/Header";
+import React from 'react'
+
 const Dashboard = () => {
   return (
     <>
-      <div className="w-full  gap-5 px-4 py-4 flex justify-center    items-center">
-        <div className=" ">
-          <Sidebar />
-        </div>
-        <div className=" px-4 py-5  rounded-3xl bg-gray-100 border w-full ">
-          <SearchHOC />
-
-          <div>
-            <Header />
-          </div>
-
-          <div className=" flex flex-col gap-4">
-            <JobCard />
-            <JobCard />
-            <JobCard />
-          </div>
-        </div>
+      <div className="w-full flex items-center">
+        bankai
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
