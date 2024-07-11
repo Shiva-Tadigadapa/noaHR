a66e1b40075ce33c6ee2d61cb449d39a55567c4d
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-07-11 21:43:54 +0530

    refined some code

--- client/src/components/Manage/Board/BoardIndex.jsx
+++ client/src/components/Manage/Board/BoardIndex.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState, Suspense } from "react";
+
+const LazyKanban = React.lazy(() => import("./Kanban"));
+const LazyListView = React.lazy(() => import("./ListView"));
 import Menu from "./Menu";
-import Kanban from "./Kanban";
 
 const BoardIndex = () => {
+  const [selectedView, setSelectedView] = useState("list");
+
+  const handleViewChange = (view) => {
+    setSelectedView(view);
+  };
+
   return (
-    <>
-      <div className="w-full h-screen bg-white border rounded flex flex-col">
-        <h1 className="mt-8 text-2xl font-semibold mx-14">327 Applications</h1>
-        <div className="mx-14">
-          <Menu />
-        </div>
-        <div className="flex-1 mt-4 overflow-auto">
-          <Kanban />
-        </div>
+    <div className="w-full h-screen bg-white border rounded flex flex-col">
+      <h1 className="mt-8 text-2xl font-semibold mx-14">327 Applications</h1>
+      <div className="mx-14">
+        <Menu onViewChange={handleViewChange} />
       </div>
-    </>
+      <div className="flex-1 mt-4 overflow-auto">
+        <Suspense fallback={<div>Loading...</div>}>
+          {selectedView === "list" ? <LazyListView /> : <LazyKanban />}
+        </Suspense>
+      </div>
+    </div>
   );
 };
 
